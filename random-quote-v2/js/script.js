d448c1d9f01548a40a3f28be51e23cdf6fdb010c
hatshepsut0 <33952502+hatshepsut0@users.noreply.github.com>
2018-01-03 21:40:42 -0800

    Added .splice() to create non-repeating loop

--- random-quote-v2/js/script.js
+++ random-quote-v2/js/script.js
@@ -4,6 +4,24 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 
 //defining variables
 var message = '';
+var red;
+var green;
+var blue;
+var usedQuotes = [];
+
+var intervalID = window.setInterval(quoteTimer, 20000);
+function quoteTimer() {
+  printQuote();
+}
+
+//function to generate random rgb color and make it the background color
+  function randomColor() {
+      var red = Math.floor(Math.random() * 256); // range is 0-255
+	    var green = Math.floor(Math.random() * 256);
+	    var blue = Math.floor(Math.random() * 256);
+	    var bgColor = "rgb(" + red + "," + green + "," + blue + ")";
+  	document.body.style.background = bgColor;
+  }
 
 //function to print quotes to page
 function print(message) {
@@ -11,11 +29,17 @@ function print(message) {
    outputDiv.innerHTML = message;
 }
 
-//function to generate random number between 0 and length of array of quotes
+/*function to splice random quote into a new array and return it,
+reload main array when all quotes have been viewed,
+generate random number between 0 and length of array of quotes*/
 function getRandomQuote() {
-  var quoteObject = quotes[ Math.floor(Math.random() * quotes.length ) ];
-  return quoteObject;
-  }
+  if (quotes.length == 0)
+    quotes = usedQuotes.splice(0, usedQuotes.length)
+  var randomNumber = quotes[ Math.floor(Math.random() * quotes.length) ];
+  var splicedQuote = quotes.splice(randomNumber, 1)[0];
+  usedQuotes.push(splicedQuote);
+  return splicedQuote;
+}
 
 //function to print quote to page
 function printQuote() {
@@ -28,17 +52,11 @@ function printQuote() {
   if (randomQuote.year !== undefined) {
     message += '<span class="year">' + randomQuote.year + '</span>';
   }
+  if(randomQuote.tags !== undefined) {
+		message += '<p class="tags">' + randomQuote.tags + '</p>';
+	}
   message += '</p>';
   print(message);
   message = '';
-  }
-
-
-//function to generate random rgb color and make it the background color
-  function randomColor() {
-      var red = Math.floor(Math.random() * 256); // range is 0-255
-	    var green = Math.floor(Math.random() * 256);
-	    var blue = Math.floor(Math.random() * 256);
-	    var bgColor = "rgb(" + red + "," + green + "," + blue + ")"; 
-  	document.body.style.background = bgColor;
+  randomColor();
   }
